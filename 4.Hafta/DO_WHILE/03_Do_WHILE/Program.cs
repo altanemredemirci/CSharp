f1376154a04ece1357d03e0f48e3b5e6f3bf3711
altanemredemirci <altan_emre_1989@hotmail.com>
2022-10-08 12:50:25 +0300

    update

---
+++ 4.Hafta/DO_WHILE/03_Do_WHILE/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Do_WHILE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan girilen değerlerin toplamı doğum yılınızı geçene kadar toplayıp hesaplayınız
+
+
+
+            //kullıcıdan alınacak 4 adet sayının toplamlarını hesaplayınız
+            //kullanıcı sayı yerine herhangi bir değer girerse kullanıcıyı yanlış değer girdiğini bilgilendiriniz
+
+
+
+            //kullanıcıdan alınacak ürün sayısına göre her bir ürünün fiyatını bir bir isteyip kullanıcııya fiyatlarının toplamlarını gösteriniz
+
+
+            //kullanıcıdan istenecek sayılar arasında tek sayı girişi olana kadar girilen sayıların toplamını ve sayısını hesaplayan do while 
+
+
+            //bilgisayarın rastgele ürettiği bir sayı sisteminde rastgele sayı değeri 30 olduğunda kullanıcıya kaç adet sayıdan sonra 30 değerinin geldiğini hesaplayınız
+            //iki basamaklı sayılar arasında seçim yaptırınız
+
+
+
+            //sistemin rastgele ürettiği sayıyı kullanıcının bulması için bir kod bloğu yazarak, eğer kullanıcının girdiği sayı üretilen sayıdan büyük ise kullanıcının büyük bir sayı girdiğini, küçük ise küçük girdiğini uyarınız ve kullanıcı 3 basamaklı bu tahmin oyununda 5 haka sahip olsun
+
+
+
+            //250-1250 arasındaki tek ve çift sayıların toplamını ayrı ayrı hesaplayınız
+        }
+    }
+}
